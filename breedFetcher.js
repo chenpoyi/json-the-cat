af560225638220fcc421bceeaf6fa014cb77937f
Paul Chen <paulchen647@gmail.com>
2020-09-24 21:28:27 +0000

    added breedFetcher.js and implemented requests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,3 +1,15 @@
 const request = require('request');
+const userArg = process.argv.slice(2);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${userArg[0]}`, (error, response, body) => {
+  //console.log(typeof body);
+  if (error) {
+    throw error;
+  }
+  if (body === '[]') {
+    console.log("Breed not found.");
+  } else {
+    const data = JSON.parse(body);
+    console.log(data[0]['description']);
+  }
 
-console.log(typeof body)
+});
